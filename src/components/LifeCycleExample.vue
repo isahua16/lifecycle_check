5bb424618a34ffade9c273a67353472c1bb3b04c
isahua16 <isaelhuard@gmail.com>
2023-04-15 10:57:18 -0600

    playing with component hooks

---
+++ src/components/LifeCycleExample.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Hello!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        beforeCreate: function()
+        {
+            console.log(`I am in beforeCreate cycle`);
+        },
+        created: function()
+        {
+            console.log(`I am in created cycle`);
+        },
+        beforeMount: function()
+        {
+            console.log(`I am in beforeMount cycle`);
+        },
+        mounted: function()
+        {
+            console.log(`I am in mounted cycle`);
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
